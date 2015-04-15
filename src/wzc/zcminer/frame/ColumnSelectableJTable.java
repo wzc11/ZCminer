c34373068f0eb8a6d4adc8d75285f7aea3555071
wzc11 <569376037@qq.com>
2015-04-15 16:03:30 +0800

    columnselect

---
+++ src/wzc/zcminer/frame/ColumnSelectableJTable.java
@@ -0,0 +1,23 @@
+package wzc.zcminer.frame;
+
+import java.awt.event.MouseAdapter;  
+import java.awt.event.MouseEvent;  
+import javax.swing.JTable;  
+import javax.swing.table.JTableHeader;  
+
+public class ColumnSelectableJTable extends JTable{  
+	public int column; 
+    public ColumnSelectableJTable(Object[][]items,Object[] headers){  
+        super(items,headers);  
+        setColumnSelectionAllowed(true);  
+        setRowSelectionAllowed(false);  
+        final JTableHeader header=getTableHeader();  
+        header.addMouseListener(new MouseAdapter(){  
+            public void mouseReleased(MouseEvent e){    
+                if(!e.isShiftDown()) clearSelection();  
+                column=header.columnAtPoint(e.getPoint());  
+                addColumnSelectionInterval(column,column);  
+            }  
+        });  
+    }  
+}  
--- src/wzc/zcminer/frame/ImportPanel.java
+++ src/wzc/zcminer/frame/ImportPanel.java
@@ -2,10 +2,13 @@ package wzc.zcminer.frame;
 
 import java.awt.BorderLayout;
 import java.awt.FlowLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.FileReader;
 import java.util.List;
 
 import javax.swing.ButtonGroup;
+import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JRadioButton;
 import javax.swing.JScrollPane;
@@ -14,7 +17,7 @@ import javax.swing.JTable;
 import com.opencsv.CSVReader;
 
 public class ImportPanel extends JPanel{
-	static JTable table;
+	static ColumnSelectableJTable table;
 	static JPanel radioPanel;
 	static JRadioButton removeButton;
 	static JRadioButton caseButton;
@@ -30,12 +33,67 @@ public class ImportPanel extends JPanel{
 			setLayout(new BorderLayout());
 			radioPanel = new JPanel();
 			radioPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
+			
 			removeButton = new JRadioButton("remove");
+			removeButton.addActionListener(new ActionListener()
+	        {
+	        	public void actionPerformed(ActionEvent e) 
+	        	{
+	        		
+	        		
+	        	}
+	        });
+			
 			caseButton = new JRadioButton("case");
+			caseButton.addActionListener(new ActionListener()
+	        {
+	        	public void actionPerformed(ActionEvent e) 
+	        	{
+	        		
+	        		
+	        	}
+	        });
+			
 			activityButton = new JRadioButton("activity");
+			activityButton.addActionListener(new ActionListener()
+	        {
+	        	public void actionPerformed(ActionEvent e) 
+	        	{
+	        		
+	        		
+	        	}
+	        });
+			
+			
 			timeButton = new JRadioButton("timestamp");
+			timeButton.addActionListener(new ActionListener()
+	        {
+	        	public void actionPerformed(ActionEvent e) 
+	        	{
+	        		
+	        		
+	        	}
+	        });
+			
 			resourseButton = new JRadioButton("resourse");
+			resourseButton.addActionListener(new ActionListener()
+	        {
+	        	public void actionPerformed(ActionEvent e) 
+	        	{
+	        		
+	        		
+	        	}
+	        });
+			
 			otherButton = new JRadioButton("other");
+			otherButton.addActionListener(new ActionListener()
+	        {
+	        	public void actionPerformed(ActionEvent e) 
+	        	{
+	        		
+	        		
+	        	}
+	        });
 			
 			radioPanel.add(removeButton);
 			radioPanel.add(caseButton);
@@ -58,10 +116,20 @@ public class ImportPanel extends JPanel{
 			List<String[]> myEntries = reader.readAll();
 			String[] headlines = myEntries.remove(0);
 			String[][] rowData = myEntries.toArray(new String[0][]);
-			table = new JTable(rowData, headlines);
+			table = new ColumnSelectableJTable(rowData, headlines);
 			JScrollPane dataPanel =new JScrollPane(table);
 			add(dataPanel,BorderLayout.CENTER);
 			reader.close();
+			
+			JButton okButton=new JButton("开始分析");
+			okButton.addActionListener(new ActionListener()
+	        {
+	        	public void actionPerformed(ActionEvent e) 
+	        	{	
+	        	}
+	        });
+			
+			add(okButton,BorderLayout.PAGE_END);
 			
 			
 		 } catch (Exception ex) {
